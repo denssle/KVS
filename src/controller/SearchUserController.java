c02c8dcad80417213912a85bf951c66ccd799a22
Dominik Enßle <dominik.enssle@innoq.com>
2015-03-31 22:59:31 +0200

    SearchUser: Init

---
+++ src/controller/SearchUserController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+import view.SearchUserView;
+
+public class SearchUserController implements ActionListener {
+	private SearchUserView searchUserView;
+	
+	public SearchUserController() {
+		searchUserView = new SearchUserView(this);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		String command = e.getActionCommand();
+		System.out.println(command);
+		if(command.equals(statics.label.ok))
+		{
+			System.out.println("Suche wird gestartet. Oder so. ");
+		}
+	}
+
+	public JPanel getPanel() {
+		return searchUserView;
+	}
+
+}
---
+++ src/view/SearchUserView.java
@@ -0,0 +1,26 @@
+package view;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+@SuppressWarnings("serial")
+public class SearchUserView extends JPanel {
+
+	public SearchUserView(ActionListener actionListener) {
+		this.setLayout(new FlowLayout());
+		
+		JLabel label = new JLabel("Suche… ");
+		JTextField textfield = new JTextField("… nach Klienten. ");
+		JButton button = new JButton(statics.label.ok);
+		button.addActionListener(actionListener);
+		
+		this.add(label);
+		this.add(textfield);
+		this.setVisible(true);
+	}
+}
