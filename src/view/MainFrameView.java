0dbe1381c183bbea68b9313faa3d3eeccc3847ba
Dominik Enßle <dominik.enssle@innoq.com>
2015-03-31 09:46:30 +0200

    JMenu Init

--- src/view/MainFrameView.java
+++ src/view/MainFrameView.java
@@ -3,16 +3,57 @@ package view;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 
+@SuppressWarnings("serial")
 public class MainFrameView extends JFrame
 {
+	private JMenuItem menuItem;
+	
 	public MainFrameView(ActionListener hauptrahmenController)
 	{
 		this.setTitle("KVS");
-		this.setSize(700,700);
+		this.setSize(java.awt.Toolkit.getDefaultToolkit().getScreenSize());
 		this.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
 		this.setAlwaysOnTop(true);
+		
+		JMenuBar menuBar = new JMenuBar();
+		
+		menuBar.add(createUserMenu());
+		menuBar.add(createEditMenu());
+		this.setJMenuBar(menuBar);
+	}
+	
+	private JMenu createUserMenu() {
+		JMenu userMenu = new JMenu("Clienten");
+		
+		menuItem = new JMenuItem("Neuen Klienten anlegen");
+		userMenu.add(menuItem);
+		
+		menuItem = new JMenuItem("Klient suchen");
+		userMenu.add(menuItem);
+		
+		userMenu.addSeparator();
+		
+		menuItem = new JMenuItem("Beenden");
+		menuItem.getAccessibleContext().setAccessibleDescription("Quit KVS");
+		userMenu.add(menuItem);
+		return userMenu;
+	}
+	
+	private JMenu createEditMenu() {
+		JMenu editMenu = new JMenu("Bearbeiten");
+		
+		menuItem = new JMenuItem("Klient bearbeiten");
+		editMenu.add(menuItem);
+		
+		menuItem = new JMenuItem("Klient löschen");
+		editMenu.add(menuItem);
+		
+		return editMenu;
 	}
 	
 	public void start()
