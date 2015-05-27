832bada25377371efd842725ef92a69b6fe0667d
reskes <res.keskin@gmail.com>
2015-05-27 12:13:43 +0200

    MySQL Update #06
    Struktur nochmal angepasst.
    Platzhalter für das sichern der Datenbankfunktionalität erstellt.

--- src/util/UserGenerator.java
+++ src/util/UserGenerator.java
@@ -1,6 +1,9 @@
+package util;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 
@@ -8,7 +11,7 @@ import model.User;
 import dao.UserDAO;
 
 
-public class CreateTestUser {
+public class UserGenerator {
 
 	private static String[] GIVEN_NAMES = {"Inge", "Rolf", "Martina", "Wolfgang", "Walter", "Frank", "Hans", "Jörg", "Andreas", "Wilhelm", "Horst", "Josef", "Günter", "Marianne", "Sabine", "Christine", "Marion", "Thomas", "Willi", "Gertrud", "Matthias", "Joachim", "Ute", "Anna", "Norbert", "Barbara", "Brigitte", "Edith", "Ingeborg", "Sandra", "Ulrike", "Irmgard", "Hermann", "Birgit", "Christa", "Stefan", "Claudia", "Bernhard", "Gisela", "Kerstin", "Markus", "Gabriele", "Ingrid", "Petra", "Susanne", "Heinrich", "Bernd", "Heinz", "Elke", "Klaus", "Erich", "Heike", "Anja", "Elfriede", "Waltraud", "Johann", "Hildegard", "Renate", "Andrea", "Georg", "Rainer", "Monika", "Martin", "Ursula", "Uwe", "Manfred", "Rudolf", "Herbert", "Anneliese", "Christian", "Dieter", "Hannelore", "Doris", "Helmut", "Ulrich", "Karl", "Peter", "Siegfried", "Gerhard", "Alexander", "Beate", "Jürgen", "Michael", "Erika", "Karl-Heinz", "Maria", "Ruth", "Margarete", "Ilse", "Ralf", "Gerda", "Kurt", "Elisabeth", "Franz", "Helga", "Angelika", "Jutta", "Werner", "Alfred", "Karin"};
 	private static String[] FAMILY_NAMES = {"Müller", "Schmidt", "Schneider", "Fischer", "Weber", "Meyer", "Wagner", "Becker", "Schulz", "Hoffmann", "Schäfer", "Bauer", "Koch", "Richter", "Klein", "Wolf", "Schröder", "Neumann", "Schwarz", "Braun", "Hofmann", "Zimmermann", "Schmitt", "Hartmann", "Krüger", "Schmid", "Werner", "Lange", "Schmitz", "Meier", "Krause", "Maier", "Lehmann", "Huber", "Mayer", "Herrmann", "Köhler", "Walter", "König", "Schulze", "Fuchs", "Kaiser", "Lang", "Weiß", "Peters", "Scholz", "Jung", "Möller", "Hahn", "Keller"};
@@ -19,18 +22,16 @@ public class CreateTestUser {
 	private static Random rnd = new Random();
 	
 	public static void main(String[] args){
-		for(int i = 0; i < 40; i++) {
-			User user = new User(UUID.randomUUID(),getRandom(GIVEN_NAMES),getRandom(FAMILY_NAMES),getRandomBirthdate(),getRandom(STREET_NAMES)+" "+randBetween(1,199),String.format("%05d", randBetween(100,99999)),getRandom(CITY_NAMES));
-			statics.debug.debugMessage("CreateTestUser", user.toString());
-			UserDAO.getInstance().addUser(user);
+		for(User u: getRandomLocalUsers(40)) {
+			UserDAO.getInstance().addUser(u);
 		}
 	}
 
-	public static String getRandom(String[] names) {
+	private static String getRandom(String[] names) {
 		return names[rnd.nextInt(names.length)];
 	}
 	
-	public static String getRandomBirthdate()
+	private static String getRandomBirthdate()
 	{
 		GregorianCalendar gc = new GregorianCalendar();
         int year = randBetween(1946, 1996);
@@ -40,7 +41,17 @@ public class CreateTestUser {
 		return String.format("%02d",gc.get(Calendar.DAY_OF_MONTH)) + "." + String.format("%02d",gc.get(Calendar.MONTH)) + "." + gc.get(Calendar.YEAR);		
 	}
 	
-    public static int randBetween(int start, int end) {
+    private static int randBetween(int start, int end) {
         return start + (int)Math.round(Math.random() * (end - start));
+    }
+    
+    public static List<User> getRandomLocalUsers(int count) {
+    	List<User> users = new ArrayList<User>();
+		for(int i = 0; i < count; i++) {
+			User user = new User(UUID.randomUUID(),getRandom(GIVEN_NAMES),getRandom(FAMILY_NAMES),getRandomBirthdate(),getRandom(STREET_NAMES)+" "+randBetween(1,199),String.format("%05d", randBetween(100,99999)),getRandom(CITY_NAMES));
+			statics.debug.debugMessage("CreateTestUser", user.toString());
+			users.add(user);
+		}
+		return users;
     }
 }
