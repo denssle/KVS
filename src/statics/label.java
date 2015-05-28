8d4747117825b5dbb101fa3c0e49b0d0205bb1ac
Dominik Enßle <dominik.enssle@innoq.com>
2015-05-28 15:04:09 +0200

    Umlaute

--- src/statics/label.java
+++ src/statics/label.java
@@ -8,15 +8,15 @@ public final class label {
 	public final static String quit = "Beenden";
 	public final static String update = "Bearbeiten";
 	public final static String updateClient = "Klient bearbeiten";
-	public final static String delete = "Löschen";
-	public final static String deleteClient = "Klient löschen";
+	public final static String delete = "LÃ¶schen";
+	public final static String deleteClient = "Klient lÃ¶schen";
 	public final static String cancel = "Abbrechen";
 	public final static String titel = "KVS";
 	public final static String showClient = "Klient anzeigen";
 	
 	public final static String forname = "Vorname";
 	public final static String lastname = "Nachname";
-	public final static String street = "Straße";
+	public final static String street = "StraÃŸe";
 	public final static String zip ="PLZ";
 	public final static String city = "Stadt";
 	public final static String birthdate = "Geburtsdatum";
