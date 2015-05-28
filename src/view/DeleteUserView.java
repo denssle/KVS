6cd2ab661797872b1bc8f06da70fbcf3026ee799
Dominik Enßle <dominik.enssle@innoq.com>
2015-05-28 10:59:58 +0200

    Debug: Lil Update

--- src/view/DeleteUserView.java
+++ src/view/DeleteUserView.java
@@ -30,7 +30,14 @@ public class DeleteUserView implements Observer{
 		if(ShowUserController.getUser() != null) {
 			// 0 = delete, 1 = Nicht löschen, 2 = cancel
 			int res = JOptionPane.showConfirmDialog(null, "Sicher?", "User löschen.",JOptionPane.YES_NO_CANCEL_OPTION);
-			statics.debug.debugMessage("DeleteUserView", "Die Antwort war: "+res);
+			switch (res) {
+            case 0:
+            	statics.debug.debugMessage("DeleteUserView", "User soll gelöscht werden");
+                break;
+            case 1:
+            	statics.debug.debugMessage("DeleteUserView", "User soll nicht gelöscht werden");
+                break;
+        }
 			DeleteUserController.deleteUser(res);
 		} else {
 			JOptionPane.showMessageDialog(null, "Kein User zum löschen offen; bitte öffnen Sie erst einen User um ihn zu löschen. ");
