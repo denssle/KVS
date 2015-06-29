a1fb3624ac2b577396f81d17c410775a8f064b73
reskes <Keskin@LAP5121.dom1.vrr.de>
2015-06-29 12:07:03 +0200

    Init Language

--- src/controller/MainFrameController.java
+++ src/controller/MainFrameController.java
@@ -4,6 +4,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Observable;
 
+import util.Local;
+
 public class MainFrameController extends Observable implements ActionListener {
 	
 	public MainFrameController() {
@@ -14,10 +16,11 @@ public class MainFrameController extends Observable implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		String command = e.getActionCommand();
 		statics.debug.debugMessage("MainFrameController",command);		
+		Local local = Local.getInstance();
 		if(command.equals(statics.label.german)) {
-			
+			local.init("de", "de");
 		} else if(command.equals(statics.label.english)) {
-			
+			local.init("en", "en");
 		}
 		setChanged(); 
 		notifyObservers(command); 
