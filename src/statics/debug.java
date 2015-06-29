c68a840208cd44a2a00e1cd8f51dba87854eacb4
Dominik Enßle <dominik.enssle@innoq.com>
2015-06-29 11:29:48 +0200

    Logger Init

--- src/statics/debug.java
+++ src/statics/debug.java
@@ -1,16 +1,25 @@
 package statics;
 
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+
 public class debug {
 	private static Integer counter = 0;
+	private static final Logger log = Logger.getLogger("confLogger");
 	
 	public debug() {
 	}
 	
 	public static void debugMessage(String sender, String message) {
-		System.out.println(getCounter()+" DEBUG: "+sender+": "+message);
+		
+		//System.out.println(getCounter()+" DEBUG: "+sender+": "+message);
+		log.log( Level.INFO, sender+" : "+message, getCounter()+message);
 	}
 	public static void errorMessage(String sender, String message) {
-		System.out.println(getCounter()+" ERROR: "+sender+": "+message);
+		//System.out.println(getCounter()+" ERROR: "+sender+": "+message);
+		log.log( Level.WARNING, sender+" : "+message, getCounter()+message);
 	}
 	private static String getCounter() {
 		counter += 1;
